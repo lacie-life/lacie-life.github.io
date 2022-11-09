a4466b19c65e38a29399ca6f9e458a751f099f1c
lacie-life <00sao00ios00@gmail.com>
2022-11-10 00:04:01 +0700

    []

--- _scripts/publish.d/example.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-echo "This is an example script that will be executed in the HTML root upon publishing."
-echo "HTML root content:" $(ls)
---
+++ _tabs/about.md
@@ -0,0 +1,9 @@
+---
+title: About
+icon: fas fa-info
+order: 4
+---
+
+I am a jellyfish freely.
+
+
